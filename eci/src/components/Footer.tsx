a2c9523dad261784fd1cbaab7e5e747462b6b1fc
VictorSama07 <louieboysalviejo06@gmail.com>
2025-12-15 15:52:12 +0800

    credit and logo

--- eci/src/components/Footer.tsx
+++ eci/src/components/Footer.tsx
@@ -1,22 +1,38 @@
 const Footer = () => {
   return (
-    <footer className="max w-full bg-gradient-to-r from-[#071a33] via-[#06122a] to-[#040c20] text-white">
-      <div className="max w-full mx-auto px-6 py-6 flex flex-col md:flex-row items-center justify-between gap-6 text-sm">
+    <footer className="w-full bg-gradient-to-r from-[#071a33] via-[#06122a] to-[#040c20] text-white">
+      <div className="w-full mx-auto px-6 py-6 flex flex-col md:flex-row items-center justify-between gap-6 text-sm">
 
         {/* Left */}
-        <div className="text-center md:text-left">
+        <div className="text-center md:text-left space-y-1">
           <p className="text-slate-300">Developed by Student of</p>
           <p className="font-semibold">
             Batangas State University JPLPC – Malvar Campus
           </p>
+
+          {/* Enhancement credit */}
+        <p className="text-slate-300">
+            Enhanced by students of Cagayan State University – Gonzaga Campus
+          </p>
+          
         </div>
 
-        {/* Center */}
-        <div className="flex justify-center">
+        {/* Center (Logos) */}
+        <div className="flex items-center justify-center gap-2">
           <img
-            src="/logo.png"
+            src="/bsu.webp"
+            alt="Batangas State University Logo"
+            className="h-10"
+          />
+          <img
+            src="/logo1.webp"
             alt="ECI Logo"
             className="h-10"
+          />
+          <img
+            src="/CSU-CICS logo.png"
+            alt="Cagayan State University Logo"
+            className="h-12"
           />
         </div>
 
