fb6b6724d1fefefc9ad93d96717a3d1c68f05ebe
Rey Tabasan <reytabasan123@gmail.com>
2026-01-08 13:38:31 +0800

    updated_Products

--- eci/src/components/Products.tsx
+++ eci/src/components/Products.tsx
@@ -125,7 +125,7 @@ export default function Products() {
                           <p 
                             className={`text-xl text-blue-400 font-semibold mb-4 transition-all duration-700 ${
                               index === currentSlide 
-                                ? 'opacity-100 translate-y-0' 
+                                ? 'opacity-100 translate-y-0'
                                 : 'opacity-0 translate-y-8'
                             }`}
                             style={{ transitionDelay: index === currentSlide ? '300ms' : '0ms' }}
