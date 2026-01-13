d888b34221fca891691a1eacd528d3c688a1075d
Rey Tabasan <reytabasan123@gmail.com>
2026-01-13 12:48:41 +0800

    add swipe hint

--- eci/src/components/Products.tsx
+++ eci/src/components/Products.tsx
@@ -177,7 +177,7 @@ export default function Products() {
                 </div>
               )}
 
-              {/* Mobile*/}
+              {/* Mobile */}
               <div
                 ref={mobileScrollRef}
                 onScroll={handleMobileScroll}
